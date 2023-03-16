d8217383f7f896284d8726cf7e83a85e48c4a9b2
MJ1406m <mukelijuliet250@gmail.com>
2023-03-16 16:39:23 -0700

    added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistid.h>
+/**
+ * main-A program that prints aline to the standard error
+ * Return 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19n", stdout);
+return (1);
+}
