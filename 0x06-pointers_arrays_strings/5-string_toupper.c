92dedadc666e388bd1e2df485d783e44c6154899
MJ1406m <125567018+MJ1406m@users.noreply.github.com>
2023-03-31 03:47:05 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,21 +1,36 @@
 #include "main.h"
 /**
- * string_toupper - change lowercase letters to uppercase.
+ * cap_string - capitalizes most of the words in a string.
  * @s: analized string.
  *
- * Return: String with all letters Uppercased.
+ * Return: String with all words capitalized.
  */
-char *string_toupper(char *s)
+char *cap_string(char *s)
 {
-	int i = 0;
+	int i, j;
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
+	i = 0;
 	while (*(s + i) != '\0')
 	{
 		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			*(s + i) = *(s + i) - 32;
+			if (i == 0)
+			{
+				*(s + i) = *(s + i) - 32;
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+					{
+						*(s + i) = *(s + i) - 32;
+					}
+				}
+			}
 		}
-		i++;
+	i++;
 	}
 	return (s);
 }
